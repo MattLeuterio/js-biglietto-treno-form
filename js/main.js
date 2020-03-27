1b35c6cb9124bae63989bfd9c969acb340a328eb
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-03-27 14:53:46 +0100

    Javascript Finished

---
+++ js/main.js
@@ -0,0 +1,91 @@
+/***************************
+ * VENDITA BIGLIETTO TRENO
+ ***************************/
+
+/** 
+ * REFERENZE ELEMENTI
+ */
+var container = document.getElementById('biglietto');
+var bottoneGenera = document.getElementById('bottoneGenera');
+var bottoneAnnulla = document.getElementById('bottoneAnnulla');
+
+
+
+/**
+ * EVENTI
+ */
+
+// Generare il biglieto
+bottoneGenera.addEventListener('click', 
+    function() {
+        // Ottieni valori input utente
+        var nome = document.getElementById('nome').value;
+        //console.log(nome); 
+
+        var kmDaPercorrere = document.getElementById('km').value;
+        //console.log(kmDaPercorrere); 
+
+        var fasciaEta = document.getElementById('fascia-eta').value;
+        //console.log(fasciaEta); 
+
+        // Calcolo biglietto
+        var prezzoKm = 0.21;
+        var costoBiglietto = prezzoKm * kmDaPercorrere;
+        var offerta = 'Biglietto Standard';
+
+        // Calcoliamo il costo e l'offerta applicata
+        if (fasciaEta == 'minorenne') {
+            // 20% sconto
+            costoBiglietto -= costoBiglietto * 0.2;
+            offerta = 'Sconto minorenne';
+        } else if (fasciaEta == 'over65') {
+            //40% sconto
+            costoBiglietto -= costoBiglietto * 0.4;
+            offerta = 'Sconto Over 65';
+        }
+
+        // Controllo dei decimali
+        costoBiglietto = costoBiglietto.toFixed(2) + '€';
+
+        // Numero random per la carrozza da 1 a 9
+        var numCarrozza = Math.floor( Math.random() * 9) + 1;
+
+        // Numero random per codice cp da 90000 a 100000
+        var codiceCp = Math.floor( Math.random() * (100000 - 90000) ) + 90000;
+
+        // Inseriamo valori nella pagina
+        document.getElementById('nome-passeggero').innerHTML = nome;
+        document.getElementById('offerta-applicata').innerHTML = offerta;
+        document.getElementById('carrozza').innerHTML = numCarrozza;
+        document.getElementById('codice-cp').innerHTML = codiceCp;
+        document.getElementById('costo').innerHTML = costoBiglietto;
+
+        // Mostra biglietto
+        container.className = 'show';
+    }   
+);
+
+// Reset biglietto
+bottoneAnnulla.addEventListener('click',
+    function() {
+        
+        // Reset input forniti dall'utente
+        var nome = document.getElementById('nome').value = '';
+        var kmDaPercorrere = document.getElementById('km').value = '';
+        var fasciaEta = document.getElementById('fascia-eta').value = 'minorenne';
+        
+        // Reset valori nella pagina
+        document.getElementById('nome-passeggero').innerHTML = '';
+        document.getElementById('offerta-applicata').innerHTML = '';
+        document.getElementById('carrozza').innerHTML = '';
+        document.getElementById('codice-cp').innerHTML = '';
+        document.getElementById('costo').innerHTML = '';
+
+        // Nascondi biglietto
+        container.className = 'hidden';
+    }
+);
+
+
+
+
